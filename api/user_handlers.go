35c4f2b103cf8faaa12041208bb720d8dcf506bd
Rojer Du <rojerdu.dev@gmail.com>
2024-02-07 16:33:00 -0800

    add Delete and Put user routes

--- api/user_handlers.go
+++ api/user_handlers.go
@@ -1,9 +1,13 @@
 package api
 
 import (
+	"errors"
 	"github.com/gofiber/fiber/v2"
 	"github.com/rojerdu-dev/hotel-reservation/db"
 	"github.com/rojerdu-dev/hotel-reservation/types"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type UserHandler struct {
@@ -14,13 +18,44 @@ func NewUserHandler(userStore db.UserStore) *UserHandler {
 	return &UserHandler{userStore: userStore}
 }
 
+func (h *UserHandler) HandlePutUser(c *fiber.Ctx) error {
+	var (
+		//values bson.M
+		params types.UpdateUserParams
+		userID = c.Params("id")
+	)
+	oid, err := primitive.ObjectIDFromHex(userID)
+	if err != nil {
+		return err
+	}
+	if err := c.BodyParser(&params); err != nil {
+		return err
+	}
+	filter := bson.M{"_id": oid}
+	if err := h.userStore.UpdateUser(c.Context(), filter, params); err != nil {
+		return err
+	}
+	return c.JSON(map[string]string{"updated": userID})
+}
+
+func (h *UserHandler) HandleDeleteUser(c *fiber.Ctx) error {
+	userID := c.Params("id")
+	if err := h.userStore.DeleteUser(c.Context(), userID); err != nil {
+		return err
+	}
+	return c.JSON(map[string]string{"deleted": userID})
+}
+
 func (h *UserHandler) HandlePostUser(c *fiber.Ctx) error {
-	var params types.CreateUserParams
+	var (
+		params     types.CreateUserParams
+		errStrings = map[string]string{}
+	)
+
 	if err := c.BodyParser(&params); err != nil {
 		return err
 	}
 
-	var errStrings []string
 	if errStrings = params.Validate(); len(errStrings) > 0 {
 		return c.JSON(errStrings)
 	}
@@ -42,7 +77,9 @@ func (h *UserHandler) HandleGetUser(c *fiber.Ctx) error {
 	)
 	user, err := h.userStore.GetUserByID(c.Context(), id)
 	if err != nil {
-		return err
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return c.JSON(map[string]string{"error": "not found"})
+		}
 	}
 	return c.JSON(user)
 }
